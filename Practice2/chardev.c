b2b82e62f56dee526767a6fcfd80385627886398
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2023-05-19 13:18:18 +0300

    Add files via upload

---
+++ Practice2/chardev.c
@@ -0,0 +1,186 @@
+#include <linux/kernel.h> 
+#include <linux/module.h> 
+#include <linux/fs.h> 
+#include <asm/uaccess.h> /* для put_user */
+#include <linux/ioctl.h>
+
+MODULE_LICENSE("GPL");
+/* 
+ * Прототипы 
+ */ 
+
+int init_module(void); 
+void cleanup_module(void); 
+
+static int device_open(struct inode *, struct file *); 
+static int device_release(struct inode *, struct file *); 
+static ssize_t device_read(struct file *, char *, size_t, loff_t *); 
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+// int device_ioctl(struct inode *inode, struct file *file, unsigned int ioctl_num, unsigned long ioctl_param);
+long device_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param);
+
+
+#define SUCCESS 0 
+#define DEVICE_NAME "chardev" /* Имя устройства добаляемое в /proc/devices */
+#define BUF_LEN 1024 /* Максимальная длина сообщения в устройстве */
+
+#define IOCTL_RESET _IOR(Major, 0, int)
+
+/*
+ * Глобальные переменные, декларированные как static, глобальные внутри файла. 
+ */
+
+static int Major; /* Major-номер, для нашего устройства */ 
+static int Device_Open = 0; /* Is device open? Используется, чтобы ограничить 
+			     * множественный доступ к устройству */ 
+static char msg[BUF_LEN]; /* The msg the device will give when asked */ 
+static char *msg_rPtr;
+static char *msg_wPtr;
+
+static struct file_operations fops = {
+	.read = device_read,
+	.write = device_write,
+    .unlocked_ioctl = device_ioctl,
+	.open = device_open,
+	.release = device_release
+};
+
+/*
+ * Эта функция вызывается, когда модуль загружается. 
+ */
+int init_module(void) { 
+	Major = register_chrdev(0, DEVICE_NAME, &fops);
+
+	if (Major < 0) { 
+		printk(KERN_ALERT "Регистрация симв. устройства failed для %d\n", Major); 
+	return Major; 
+	}
+
+	printk(KERN_INFO "Был присвоен major-номер %d.\n", Major); 
+	printk(KERN_INFO "To talk to the driver, create a dev file with\n"); 
+	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major); 
+	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n"); 
+	printk(KERN_INFO "the device file.\n"); 
+	printk(KERN_INFO "Remove the device file and module when done.\n");
+	sprintf(msg, "Some msg!\n");
+
+	return SUCCESS;
+}
+
+/* 
+ * Эта функция вызывается, когда модуль выгружается. 
+ */ 
+void cleanup_module(void) {
+	/* 
+	 * Unregister the device 
+	 */
+	unregister_chrdev(Major, DEVICE_NAME); 
+}
+
+/* 
+ * Методы:
+ * Вызывается, когда процесс открывает файл устройства, подобно: 
+ * "cat /dev/mycharfile" */ 
+static int device_open(struct inode *inode, struct file *file) { 
+	static int counter = 0;
+
+	if (Device_Open) 
+		return -EBUSY;
+
+	Device_Open++; 
+	printk("Я уже сообщил тебе %d раз Hello world!\n", counter++); 
+	printk("Opening /dev/chardev process name is %s\n", current->comm);
+	msg_rPtr = msg; 
+	msg_wPtr = msg;
+	try_module_get(THIS_MODULE);
+
+	return SUCCESS;
+}
+
+/* 
+ * Вызывается, когда процесс закрывает файл устройства.
+ */ 
+static int device_release(struct inode *inode, struct file *file) { 
+	Device_Open--; /* Мы теперь готовы для следующего вызова */
+
+	/* 
+	 * Decrement the usage count, or else once you opened the file, you'll 
+	 * never get get rid of the module.
+	 */ 
+	module_put(THIS_MODULE);
+
+	return 0;
+}
+
+/* 
+ * Вызывается, когда процесс, который уже открыл файл устройства, 
+ * пытается читать из него.
+ */ 
+static ssize_t device_read(struct file *filp, /* смотри include/linux/fs.h */
+			   char *buffer, /* буфер для данных */ 
+			   size_t length, /* длина буфера */ 
+			   loff_t * offset) /* смещение */ {
+
+
+	/* 
+	 * Число байт, реально записанных в буфер 
+	 */
+
+	int bytes_read = 0;
+
+	/* 
+	 * Если конец сообщения, то
+	 * return 0, обозначая конец файла 
+	 */ 
+	if (*msg_rPtr == 0) 
+		return 0;
+
+	/* 
+	 * Фактическая посылка данных в буфер 
+	 */ 
+	while (length && *msg_rPtr) {
+
+		/* 
+		 * Буфер находится в user data segment, not the kernel 
+		 * segment so "*" assignment won't work. Мы можем использовать 
+		 * put_user, которая копирует данные из сегмента данных ядра в 
+		 * сегмент данных пользователя.
+		 */ 
+		put_user(*(msg_rPtr++), buffer++);
+
+		length--; 
+		bytes_read++;
+
+	}
+
+	/* 
+	 * Большинство функций чтения возвращают число байт, посланных в буфер 
+	 */ 
+	return bytes_read;
+}
+
+
+static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off) {
+    int i;
+    for(i=0; i<len && i<BUF_LEN; i++)
+    get_user(msg[i], buff+i);
+    msg_wPtr = msg;
+    return i;
+}
+
+
+long device_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param){
+    int i;
+ 
+    printk(KERN_INFO "Device IOCTL");
+/*    switch (ioctl_num) {
+        case IOCTL_RESET:
+*/
+            char buf[BUF_LEN];
+            for (i = 0; i < BUF_LEN; i++)
+                buf[i] = ' ';
+            
+            device_write(file, buf, i, 0);
+    //}
+    return 0;
+}
---
+++ Practice3/chardev.c
@@ -0,0 +1,128 @@
+#include <linux/module.h> 
+#include <linux/kernel.h> 
+#include <linux/wait.h> 
+#include <linux/kthread.h>
+#include <linux/delay.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/errno.h>
+#include <linux/sched.h>
+#include <asm/uaccess.h>
+#include <linux/stat.h>
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BairDobylov");
+MODULE_DESCRIPTION("Waiting Process Demo");
+
+#define FIRST_MINOR 0
+#define MINOR_CNT 1
+#define TRUE 1
+#define FALSE 0
+#define BUF_LEN 1024
+
+static int counter = 0;
+static bool thr_lock = FALSE; 
+static dev_t dev;
+static struct cdev c_dev;
+static struct class *cl;
+static DECLARE_WAIT_QUEUE_HEAD(wq);
+static char msg[BUF_LEN]; 
+static char *msg_Ptr;
+
+
+struct task_struct *ts; 
+int thread(void *data) { 
+    while(1) {
+        msleep(100);
+        sprintf(msg, "%d\n",counter);
+        msg_Ptr = msg;
+        if (thr_lock == FALSE) {
+            counter++;
+            printk("Hello. I am kernel thread! counter=%d\n", counter);
+            msleep(5000);
+            thr_lock = TRUE;
+            wake_up_interruptible(&wq);
+        } 
+    } 
+    return 0; 
+} 
+
+ssize_t read(struct file *filp, char *buffer, size_t length, loff_t *offset) 
+{
+    printk(KERN_INFO "Inside read\n");
+    printk(KERN_INFO "Scheduling Out\n");
+    wait_event_interruptible(wq, thr_lock == TRUE);
+    thr_lock = FALSE;
+    printk(KERN_INFO "Woken Up\n");
+    copy_to_user(buffer, msg, BUF_LEN);
+    return length;
+}
+
+int open(struct inode *inode, struct file *filp)
+{
+    printk(KERN_INFO "Inside open\n");
+    try_module_get(THIS_MODULE);
+    return 0;
+}
+
+int release(struct inode *inode, struct file *filp) 
+{
+    printk (KERN_INFO "Inside close\n");
+    module_put(THIS_MODULE);
+    return 0;
+}
+
+struct file_operations pra_fops = {
+    read:        read,
+    open:        open,
+    release:     release
+};
+
+int init_module(void) { 
+    printk(KERN_INFO "init_module() called\n");
+
+    int ret;
+    struct device *dev_ret;
+
+    if ((ret = alloc_chrdev_region(&dev, FIRST_MINOR, MINOR_CNT, "SCD")) < 0)
+    {
+        return ret;
+    }
+    printk("Major Nr: %d\n", MAJOR(dev));
+
+    cdev_init(&c_dev, &pra_fops);
+
+    if ((ret = cdev_add(&c_dev, dev, MINOR_CNT)) < 0)
+    {
+        unregister_chrdev_region(dev, MINOR_CNT);
+        return ret;
+    }
+
+    if (IS_ERR(cl = class_create(THIS_MODULE, "chardrv")))
+    {
+        cdev_del(&c_dev);
+        unregister_chrdev_region(dev, MINOR_CNT);
+        return PTR_ERR(cl);
+    }
+    if (IS_ERR(dev_ret = device_create(cl, NULL, dev, NULL, "chardev")))
+    {
+        class_destroy(cl);
+        cdev_del(&c_dev);
+        unregister_chrdev_region(dev, MINOR_CNT);
+        return PTR_ERR(dev_ret);
+    }
+
+    ts=kthread_run(thread,NULL,"foo kthread");
+    return 0;
+} 
+
+void cleanup_module(void) { 
+    kthread_stop(ts); 
+    printk(KERN_INFO "cleanup_module() called\n"); 
+    device_destroy(cl, dev);
+    class_destroy(cl);
+    cdev_del(&c_dev);
+    unregister_chrdev_region(dev, MINOR_CNT);
+}
